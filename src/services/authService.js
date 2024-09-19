27e13f31132a4f956283e4b38f08c2bbafda4e8a
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2024-09-19 14:15:48 +0300

    Update deps and fix regexp (#31)
    * Update deps
    * fix regexp

--- src/services/authService.js
+++ src/services/authService.js
@@ -40,7 +40,7 @@ export function validateNewPassword(newPassword, newPasswordConfirmation) {
     };
   }
 
-  if (/[^_$!?,.*-A-Za-zÀ-ȕ0-9_$!?,.*\-]/.test(newPassword)) {
+  if (/[^_$!?,.*A-Za-zÀ-ȕ0-9_$!?,.*\-]/ug.test(newPassword)) {
     return {
       valid: false,
       error: 'Uusi salasana sisältää erikoismerkkejä, jotka eivät ole sallittuja'
